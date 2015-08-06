70cc86ef34522201280b386ed5a3b464ca4b5afe
Christoph Buelter <c.buelter@arcor.de>
2015-08-06 22:10:13 +0200

    Fix package paths.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
 
+
 setuptools.setup(name="maxstick",
                  version="0.1.0",
                  description="Control 3ds Max with a joystick.",
                  long_description=open("README.rst").read(),
                  author="Christoph Buelter",
-                 packages=["libs", "maxstick"],
-                 install_requires=[]  # libs/pyglet.
+                 packages=["libs", "libs.pyglet", "maxstick"],
+                 install_requires=[]  # libs/.pyglet
                  )
